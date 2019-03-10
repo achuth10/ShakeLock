22e92d70704eb791eb5a13cadda9425fea911f7f
achuth10 <43288837+achuth10@users.noreply.github.com>
2019-03-10 22:32:14 +0530

    Uses Seismic

--- app/src/main/java/com/example/achuth/shakelock/LockService.java
+++ app/src/main/java/com/example/achuth/shakelock/LockService.java
@@ -3,31 +3,66 @@ package com.example.achuth.shakelock;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import com.squareup.seismic.ShakeDetector;
+
+import java.io.IOException;
 
 import static com.example.achuth.shakelock.App.CHANNEL_ID;
 
-public class LockService extends Service {
+public class LockService extends Service implements ShakeDetector.Listener{
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private ShakeDetector sd;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sd = new com.squareup.seismic.ShakeDetector(this);
+        sd.start(mSensorManager);
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Lock Service")
+                .setContentText("ShakeLock is running")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-    //Intent notificationIntent = new Intent(this,MainActivity.class);
-    //PendingIntent pendingIntent = PendingIntent.getActivity(this,
-           // 0,notificationIntent,0);
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Notification notification= new NotificationCompat.Builder(this,CHANNEL_ID)
-                .setContentTitle("Lock Service")
-                .setContentText("ShakeLock is running")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                //.setContentIntent(pendingIntent)
-                .build();
-        startForeground(1,notification);
+        return START_REDELIVER_INTENT;
+    }
 
-    return START_REDELIVER_INTENT;
+    @Override
+    public void hearShake() {
+        System.out.println("Shook");
+        try {
+            Runtime.getRuntime().exec("su -c /system/bin/input keyevent 26");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- app/src/main/java/com/example/achuth/shakelock/MainActivity.java
+++ app/src/main/java/com/example/achuth/shakelock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.achuth.shakelock;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         button = (Button) findViewById(R.id.reboot);
         start=(Button)findViewById(R.id.startservice);
         stop=(Button)findViewById(R.id.stopservice);
@@ -60,39 +61,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
-//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        mAccelerometer = mSensorManager
-//                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        mShakeDetector = new ShakeDetector();
-//        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
-//
-//            public void onShake(int count) {
-//                /*
-//                 * The following method, "handleShakeEvent(count):" is a stub //
-//                 * method you would use to setup whatever you want done once the
-//                 * device has been shook.
-//                 */
-//                try {
-//                    Runtime.getRuntime().exec("su -c /system/bin/input keyevent 26");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//
-//    }
-//    public void onResume() {
-//        super.onResume();
-//        // Add the following line to register the Session Manager Listener onResume
-//        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        // Add the following line to unregister the Sensor Manager onPause
-//        mSensorManager.unregisterListener(mShakeDetector);
-//        super.onPause();
-//    }
+
 
 }
 
